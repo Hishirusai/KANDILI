1636c9dcf58b7fb86f61f4e11f87f24c111b6346
Sai010ritso <sairitso@gmail.com>
2026-04-26 19:51:45 +0800

    Buttons to redicrect to guides (flood and typhoon)

--- app/src/main/java/com/example/kandili/DashboardActivity.java
+++ app/src/main/java/com/example/kandili/DashboardActivity.java
@@ -38,20 +38,16 @@ public class DashboardActivity extends AppCompatActivity {
             if (id == R.id.nav_news) {
                 startActivity(new Intent(DashboardActivity.this, NewsActivity.class));
                 finish();
-            }
-            else if (id == R.id.nav_disasters) {
+            } else if (id == R.id.nav_disasters) {
                 startActivity(new Intent(DashboardActivity.this, DisastersActivity.class));
                 finish();
-            }
-            else if (id == R.id.nav_hotlines) {
+            } else if (id == R.id.nav_hotlines) {
                 startActivity(new Intent(DashboardActivity.this, HotlinesActivity.class));
                 finish();
-            }
-            else if (id == R.id.nav_settings) {
+            } else if (id == R.id.nav_settings) {
                 startActivity(new Intent(DashboardActivity.this, SettingsActivity.class));
                 finish();
-            }
-            else if (id == R.id.nav_logout) {
+            } else if (id == R.id.nav_logout) {
                 finish();
             }
 
